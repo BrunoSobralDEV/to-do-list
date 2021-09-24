f15a63932fdc3a28192f4a999f6978cb22f81f6d
BrunoSobralDEV <brunosobralss@hotmail.com>
2021-09-24 18:50:00 -0300

    desafio ok

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,37 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    //criar estado temporário - objeto com mesmo formato da interface
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    //setState em forma de callback. old = valor antigo
+    //... spredOperator - não esvaziar o array, pega tudo que tinha antes
+    setTasks(oldState => [...oldState, newTask])
+    setNewTaskTitle('');
+    
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id == id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    //filtrar o array
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    //salvar no estado
+    setTasks(filteredTasks);
   }
 
   return (
